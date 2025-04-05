5f03c273a17e4bf882df4c3a81acf45f8ce21406
Austin Kearns <59812315+Lillenne@users.noreply.github.com>
2025-04-04 22:00:26 -0700

    Support different exts through envvars

--- magick.lua
+++ magick.lua
@@ -1,5 +1,18 @@
+function NullOrEmpty(extstr)
+  return extstr == nil or extstr == ''
+end
+
 local ftype = "png"
 local mimetype = "image/png"
+local tostr = os.getenv("PANDOC_MAGICK_TO_EXT")
+local tomime = os.getenv("PANDOC_MAGICK_TO_MIME")
+print(tostr)
+print(tomime)
+if (not NullOrEmpty(tostr) and not NullOrEmpty(tomime)) then
+  ftype = tostr
+  mimetype = tomime
+  print("Converting to " .. ftype .. " (" .. mimetype .. ")")
+end
 
 function GetFileName(url)
   return url:match("^(.+)%..+$")
@@ -11,14 +24,23 @@ end
 
 function is_img(item)
   local lower = string.lower(item)
-  local exts = {
-    "png",
-    -- "jpg",
-    -- "jpeg",
-    "tif",
-    "tiff",
-    "webp",
-  }
+  local exts = {}
+  local extstr = os.getenv("PANDOC_MAGICK_EXTENSIONS")
+  if NullOrEmpty(extstr) then
+    exts = {
+      "png",
+      "jpg",
+      "jpeg",
+      "tif",
+      "tiff",
+      "webp",
+    }
+  else
+    for str in string.gmatch(extstr, "([^,]+)") do
+      str, _ = str:gsub("%s+", "")
+      table.insert(exts, str)
+    end
+  end
   for i = 1, #exts do
     if lower == exts[i] then
       return true
